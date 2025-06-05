34d52944ebc31ef12f4ddb553c169b1239da6798
liuyunhai <qjxlyh@icloud.com>
2025-06-05 12:14:00 +0800

    temp save

--- host/drivers/nvmq/trace.h
+++ host/drivers/nvmq/trace.h
@@ -166,11 +166,11 @@ TRACE_EVENT(nvmq_sq,
 
 #endif /* _TRACE_NVME_H */
 
-#undef TRACE_INCLUDE_PATH
+#ifndef TRACE_INCLUDE_PATH
 #define TRACE_INCLUDE_PATH /mnt/suda/host/drivers/nvmq
 #undef TRACE_INCLUDE_FILE
 #define TRACE_INCLUDE_FILE trace
 
 /* This part must be outside protection */
 #include <trace/define_trace.h>
-
+#endif
